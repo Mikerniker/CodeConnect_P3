d173d07b20e536496f2f530741114d171b0fb2f8
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-08-21 23:37:55 +0800

    initial slider

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const sliderWrapper = document.querySelector('.slider-wrapper');
     const cards = Array.from(sliderWrapper.getElementsByClassName('carousel__card'));
@@ -34,46 +36,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Move the slider at regular intervals
     setInterval(moveSlider, 2000); // Adjust the timing as needed
 });
-
-
-
-// OLD
-// document.addEventListener('DOMContentLoaded', function() {
-//     const sliderWrapper = document.querySelector('.slider-wrapper');
-//     const images = Array.from(sliderWrapper.getElementsByClassName('carousel__card'));
-//     const totalImages = images.length;
-
-//     // Clone images for an infinite loop effect
-//     images.forEach(image => {
-//         const clone = image.cloneNode(true);
-//         sliderWrapper.appendChild(clone);
-//     });
-
-//     // Set the slider's width to account for the total number of images (original + cloned)
-//     sliderWrapper.style.width = `${totalImages * 200}%`;
-
-//     // Animation loop
-//     let currentIndex = 0;
-
-//     function startSlider() {
-//         currentIndex++;
-//         if (currentIndex >= totalImages) {
-//             // Reset to start for infinite loop effect
-//             currentIndex = 0;
-//             sliderWrapper.style.transition = 'none'; // Disable transition to avoid flickering
-//             sliderWrapper.style.transform = 'translateX(0)'; // Move to the start
-//             setTimeout(() => {
-//                 sliderWrapper.style.transition = 'transform 2s linear'; // Re-enable transition
-//                 startSlider();
-//             }, 20); // Small delay to allow the reset without flicker
-//         } else {
-//             sliderWrapper.style.transform = `translateX(-${(100 / totalImages) * currentIndex}%)`;
-//             setTimeout(startSlider, 2000); // Adjust the timing for slide duration
-//         }
-//     }
-
-//     startSlider();
-// });
 
 
 //OLDER
