7cf630ed2faf236366359ba901f94c30e5a867a3
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-08-28 01:28:16 +0800

    Fix images, cards, card alignment, and slider

--- script.js
+++ script.js
@@ -107,45 +107,4 @@ addEntry();
   
 
 
-// CODE FOR SLIDER ---->> FOR REVIEW
-
-document.addEventListener('DOMContentLoaded', () => {
-    const sliderWrapper = document.querySelector('.slider__wrapper');
-    const sliderCards = document.querySelectorAll('.slider__card');
-    const totalCards = sliderCards.length / 2; // Half are clones ??
-
-    let scrollWidth = sliderWrapper.scrollWidth / 2;
-
-    function resetSlider() {
-        sliderWrapper.style.transition = 'none';
-        sliderWrapper.style.transform = 'translateX(0)';
-        setTimeout(() => {
-            sliderWrapper.style.transition = 'transform 20s linear';
-            sliderWrapper.style.transform = `translateX(-${scrollWidth}px)`;
-        }, 20);
-    }
-
-    sliderWrapper.style.transform = `translateX(-${scrollWidth}px)`;
-    sliderWrapper.addEventListener('transitionend', resetSlider);
-});
-
-
-
-//SLIDER CODE -- OLDER FOR REVIEW
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const sliderWrapper = document.querySelector('.slider-wrapper');
-//     const images = Array.from(sliderWrapper.getElementsByClassName('carousel__card'));
-    
-//     // Clone images for an infinite loop effect
-//     images.forEach(image => {
-//         const clone = image.cloneNode(true);
-//         sliderWrapper.appendChild(clone);
-//     });
-
-   
-// });
-
-
-
 
